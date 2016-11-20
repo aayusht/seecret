dc0eab5d1fc7f61f18e9ad82683ad9cfa15442df
aneeshjindal <aneesh.jindal@gmail.com>
2016-11-20 07:54:54 -0800

    Began implementation of database

--- app/src/main/java/com/seecret/mdb/seecret/NotificationService.java
+++ app/src/main/java/com/seecret/mdb/seecret/NotificationService.java
@@ -6,6 +6,9 @@ import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
 import android.text.SpannableString;
 import android.util.Log;
+import android.util.Pair;
+
+import com.mdb.easqlitelib.EaSQLite;
 
 /**
  * Created by Aayush on 11/20/2016.
@@ -13,6 +16,9 @@ import android.util.Log;
 
 public class NotificationService extends NotificationListenerService{
     Context context;
+
+    public static final String TITLE_COLUMN = "title";
+    public static final String TEXT_COLUMN = "text";
 
     @Override
     public void onCreate() {
@@ -29,6 +35,9 @@ public class NotificationService extends NotificationListenerService{
         Log.i("Tag", "" + sbn.getTag());
         Log.i("time", "" + sbn.getPostTime());
 
+        String notificationTag = sbn.getTag();
+        notificationTag = "table" + notificationTag.split(":")[1];
+
 
         String text = "";
         String title = "";
@@ -42,6 +51,25 @@ public class NotificationService extends NotificationListenerService{
         Log.i("Package",pack);
         Log.i("Title",title);
         Log.i("Text",text);
+
+        try {
+            EaSQLite.initialize(getApplicationContext());
+            if (!EaSQLite.getTableNames().contains(notificationTag)) {
+                EaSQLite.createTable(notificationTag);
+                EaSQLite.addColumn(notificationTag, TITLE_COLUMN, "TEXT");
+                EaSQLite.addColumn(notificationTag, TEXT_COLUMN, "TEXT");
+            }
+
+            Pair<String, Object> titlePair = new Pair<>(TITLE_COLUMN, (Object) title);
+            Pair<String, Object> textPair = new Pair<>(TEXT_COLUMN, (Object) text);
+            Pair<String, Object>[] pairs = new Pair[2];
+            pairs[0] = titlePair;
+            pairs[1] = textPair;
+            EaSQLite.addRow(notificationTag, pairs);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
