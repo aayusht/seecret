4cf304eaee919f6d84b4dcf4a7ae531ba6599913
aayusht <tyagia@berkeley.edu>
2016-11-20 02:12:15 -0800

    first commit

--- app/src/main/java/com/seecret/mdb/seecret/MessagesActivity.java
+++ app/src/main/java/com/seecret/mdb/seecret/MessagesActivity.java
@@ -3,11 +3,11 @@ package com.seecret.mdb.seecret;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MessagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_messages);
     }
 }
